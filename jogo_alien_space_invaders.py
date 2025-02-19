c8926ce16c7a4b9da90c0d58a7acbe0422e3e76f
Eduarda Medeiros <dudamdemedeiros@gmail.com>
2025-02-19 10:33:47 -0300

    tela inicial

--- jogo_alien_space_invaders.py
+++ jogo_alien_space_invaders.py
@@ -13,18 +13,27 @@ def desenhar_texto(texto,fonte,y):
     screen.blit(surface_texto, (x, y))
 fonte_grande = pygame.font.Font(None,35)
 fonte_pequena = pygame.font.Font(None,33)
+fonte_micro = pygame.font.Font(None,30)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
     screen.fill((0,0,0))
-    desenhar_texto("Derrote os aliens inimigos",fonte_pequena,30)
-    desenhar_texto("antes que o tempo acabe",fonte_pequena,60)
-    desenhar_texto("e vença o jogo!",fonte_grande,87)
+    desenhar_texto("Derrote os aliens inimigos",fonte_pequena,50)
+    desenhar_texto("antes que o tempo acabe",fonte_pequena,80)
+    desenhar_texto("e vença o jogo!",fonte_grande,107)
+    desenhar_texto("Você está pronto(a) para a batalha?",fonte_grande,350)
+    desenhar_texto("Clique ENTER para começar o jogo",fonte_micro,390)
 
-    alien = pygame.image.load()
-    
+    alien = pygame.image.load("alien.png").convert_alpha()
+    alien = pygame.transform.scale(alien, (474//2.5,353//2.5))
+    x_centro = (640//2)-(alien.get_width()//2)
+    y_centro = (480//2)-(alien.get_height()//2)
+    screen.blit(alien,(x_centro,y_centro))
+
+
+
     pygame.display.flip()
     clock.tick(60)
 
