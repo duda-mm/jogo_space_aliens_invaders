21980fc65710d21484385710ff6cd009e27a712c
Eduarda Medeiros <dudamdemedeiros@gmail.com>
2025-02-20 16:33:41 -0300

    criando os aliens

--- jogo_alien_space_invaders.py
+++ jogo_alien_space_invaders.py
@@ -4,19 +4,39 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
+fonte_pixel_grande = pygame.font.Font("Minecraftia-Regular.ttf", 23)
+fonte_pixel_micro = pygame.font.Font("Minecraftia-Regular.ttf", 20)
+
+alien_img = pygame.image.load("alien.png").convert_alpha()
+alien_img = pygame.transform.scale(alien_img, (int(474/10), int(353/10))) 
+
+def criar_aliens():
+    aliens = []
+    for linha in range(3):  
+        for coluna in range(8):
+            x = 60 + coluna * 60 
+            y = 60 + linha * 60  
+            aliens.append((x, y))
+    return aliens
+
+def desenhar_aliens(tela, aliens):
+    for alien in aliens:
+        tela.blit(alien_img, (alien[0], alien[1]))
+
 def jogo():
     tela_jogo = pygame.display.set_mode((640, 480))
+    aliens = criar_aliens()
     rodando = True
     while rodando:
+        tela_jogo.fill((0, 0, 0)) 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                rodando = False
+                pygame.quit()
+                exit()
         
-        tela_jogo.fill((0, 0, 0)) 
+        desenhar_aliens(tela_jogo, aliens)
         pygame.display.flip()
-    
-    pygame.quit()
-    exit()
+        clock.tick(60)
 
 def desenhar_texto(texto, fonte, y):
     surface_texto = fonte.render(texto, True, 'white')
@@ -33,8 +53,6 @@ while executando:
                 jogo()
     
     screen.fill((0, 0, 0))
-    fonte_pixel_grande = pygame.font.Font("Minecraftia-Regular.ttf", 23)
-    fonte_pixel_micro = pygame.font.Font("Minecraftia-Regular.ttf", 20)
 
     desenhar_texto("Derrote os aliens inimigos", fonte_pixel_grande, 50)
     desenhar_texto("antes que o tempo acabe", fonte_pixel_grande, 80)
@@ -43,10 +61,11 @@ while executando:
     desenhar_texto("Clique ENTER para começar o jogo", fonte_pixel_micro, 390)
     
     alien = pygame.image.load("alien.png").convert_alpha()
-    alien = pygame.transform.scale(alien, (int(474 / 2.5), int(353 / 2.5)))
-    x_centro = (640 // 2) - (alien.get_width() // 2)
-    y_centro = (480 // 2) - (alien.get_height() // 2)
+    alien = pygame.transform.scale(alien, (int(474/2.5), int(353/2.5)))
+    x_centro = (640 // 2)-(alien.get_width() // 2)
+    y_centro = (480 // 2)-(alien.get_height() // 2)
     screen.blit(alien, (x_centro, y_centro))
+
     
     pygame.display.flip()
     clock.tick(60)
